bcbaaf2de8d5df333d0ba936b59f2ffe9cf5f29e
Prince Kumar <152162433+bringthelight@users.noreply.github.com>
2025-04-28 15:47:06 +0530

    added hashed password

--- auth/auth.py
+++ auth/auth.py
@@ -1,51 +1,50 @@
-from flask import render_template, request, redirect, url_for, flash, session
-from flask import Blueprint
+from flask import render_template, request, redirect, url_for, flash, session, abort, Blueprint
 from flask_mysqldb import MySQL
+from auth.util import hash_pass, verify_pass  
 
 auth_bp = Blueprint('auth_bp', __name__)
-
 mysql = MySQL()
 
-@auth_bp.route('/', methods=['GET','POST'])
+@auth_bp.route('/', methods=['GET', 'POST'])
 def dashboard():
     cur = mysql.connection.cursor()
-
     if request.method == "GET":
         cur.execute("SELECT * FROM master_medicine_type")
         med = cur.fetchall()
-        return render_template('dashboard.html', data = med)
-
+        return render_template('dashboard.html', data=med)
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
+
         cur = mysql.connection.cursor()
         cur.execute("SELECT * FROM pharmacy_users WHERE email = %s", (email,))
         user = cur.fetchone()
 
-        if user:
-            session['loggedin']=True
+        if user and verify_pass(password, user['password']):  
+            session['loggedin'] = True
             session['user'] = user['email']
             session['user_id'] = user['id']
-        return redirect(url_for('auth_bp.dashboard'))
-    else:
-        flash('Incorrect email or password, please enter correct details')
+            return redirect(url_for('auth_bp.dashboard'))
+        else:
+            flash('Incorrect email or password, please enter correct details')
     return render_template('sign-in.html')
 
-@auth_bp.route('/signup', methods = ['GET','POST'])
+@auth_bp.route('/signup', methods=['GET', 'POST'])
 def signup():
-
-    cur = mysql.connection.cursor()
-
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
 
-        cur.execute("INSERT INTO master_medicine_type (name, email, password) VALUES (%s, %s, %s)", (name, email, password))
+        hashed_password = hash_pass(password)  
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO pharmacy_users (name, email, password) VALUES (%s, %s, %s)", 
+                    (name, email, hashed_password))
         mysql.connection.commit()
 
-
-    return render_template('sign-up.html')
+        return redirect(url_for("auth_bp.login"))
+    return render_template('sign-up.html')  
