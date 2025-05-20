68a808966bb94c8595063ea732283131bdbc7fca
idileepbaghel <dileepbdn1598@gmail.com>
2025-05-20 15:27:09 +0530

    arranging all blueprints

--- app.py
+++ app.py
@@ -16,48 +16,9 @@ def create_app(config_class=DevelopmentConfig):
     # Initialize extensions
     mysql.init_app(app)
 
-    # Register blueprints
-    from auth.auth import auth_bp
-    from medicine.medicine import medicine_bp
-    from delete_edit.delete import delete_bp
-    from delete_edit.edit import edit_bp
-    from medicine.med_type import med_bp
-    from MedNames.med_names import med_name
-    from data_fetch.route import data_fetch
-    from action.edit import edit
-    from login.pharmacy_users import users_bp
-    from medicine.medicine_brand import brand_bp
-    from medicine.pharmacy_med import pharm_name
-    from medicine.medicine_stock import medstock
-    from medicine.med_ratelist import ratelist
-    from Billing.billing_route import billing
-    from Billing.stock_report import lowstock
-    from Billing.business_report import report_bp
-    from medicine.pharmacy_details import pharm_details
-    from reports.expiry_report import expiry_report
-    from action.api import api_bp
-    from action.todo import todo
-
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(data_fetch)
-    app.register_blueprint(edit)
-    app.register_blueprint(medicine_bp)
-    app.register_blueprint(delete_bp)
-    app.register_blueprint(edit_bp)
-    app.register_blueprint(med_bp)
-    app.register_blueprint(med_name)
-    app.register_blueprint(pharm_name)
-    app.register_blueprint(users_bp)
-    app.register_blueprint(brand_bp)
-    app.register_blueprint(medstock)
-    app.register_blueprint(ratelist)
-    app.register_blueprint(billing)
-    app.register_blueprint(pharm_details)
-    app.register_blueprint(lowstock)
-    app.register_blueprint(report_bp)
-    app.register_blueprint(api_bp)
-    app.register_blueprint(todo)
-    app.register_blueprint(expiry_report)
+    # Register all blueprints
+    from blueprints import register_blueprints
+    register_blueprints(app)
 
     return app
 
---
+++ blueprints.py
@@ -0,0 +1,48 @@
+from auth.auth import auth_bp
+from medicine.medicine import medicine_bp
+from delete_edit.delete import delete_bp
+from delete_edit.edit import edit_bp
+from medicine.med_type import med_bp
+from MedNames.med_names import med_name
+from data_fetch.route import data_fetch
+from action.edit import edit
+from login.pharmacy_users import users_bp
+from medicine.medicine_brand import brand_bp
+from medicine.pharmacy_med import pharm_name
+from medicine.medicine_stock import medstock
+from medicine.med_ratelist import ratelist
+from Billing.billing_route import billing
+from Billing.stock_report import lowstock
+from Billing.business_report import report_bp
+from medicine.pharmacy_details import pharm_details
+from reports.expiry_report import expiry_report
+from action.api import api_bp
+from action.todo import todo
+
+def register_blueprints(app):
+    """Register all blueprints for the application"""
+    blueprints = [
+        auth_bp,
+        data_fetch,
+        edit,
+        medicine_bp,
+        delete_bp,
+        edit_bp,
+        med_bp,
+        med_name,
+        pharm_name,
+        users_bp,
+        brand_bp,
+        medstock,
+        ratelist,
+        billing,
+        pharm_details,
+        lowstock,
+        report_bp,
+        api_bp,
+        todo,
+        expiry_report
+    ]
+    
+    for blueprint in blueprints:
+        app.register_blueprint(blueprint)
